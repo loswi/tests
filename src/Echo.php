b8bd1049183c13c39b9daef07d41967699f8cc63
loswi@sina.com <loswi@sina.com>
2020-12-24 11:19:10 +0800

    first commit

---
+++ src/Echo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace friker\test;
+
+class Echo
+{
+	public function xml($data = array())
+	{
+	    header("Content-type:application/json");
+	    echo '<?xml version="1.0" encoding="utf-8"?>'
+	    echo '<data>';
+	    foreach($data as $k => $v){
+	       echo "<{$k}>$v</$k>";
+	    }
+	    echo '</data>';
+	    exit;
+	}
+
+	public function json($data = array())
+	{
+	    header("Content-type:application/json");
+	    echo json_encode($data);
+	    exit;
+	}
+
+}
+
